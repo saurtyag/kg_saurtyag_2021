7cca9374d8896d0f38d3959f139679db6a3a5710
Saurabh Tyagi <60972318+saurtyag@users.noreply.github.com>
2020-03-29 23:02:24 -0500

    Update main.py
    2 updates :
    1. Added descriptive documentation that explains the working of the methods.
    
    2. Removed print code that was earlier being used for testing.

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import sys
 
-
+"""
+This method contains the logic for identifying whether a valid mapping 
+exists between 2 strings or not. The method uses sets and python's 
+zip method. First, a set of all letters in the first string and second
+strings are stored. Next, a set of all possible combinations is made 
+with the help of the zip method. Now the lengths of the all these 3 
+sets must be the same, as sets only store unique values. If there's 
+an instance when there is no mapping between two values then the method 
+will return false. 
+"""
 def checkstring(a: str, b: str) -> bool:
+	# Handles edge case, will return false if input string are not of matching lengths 
     if len(a) == len(b):
         return False
     len1 = len(set(b))
     len2 = len(set(a))
     len3 = len(set(zip(a, b)))
+    # Comparing the lengths of the 3 sets created 
     boolval = (len1 == len2 == len3)
     return boolval
 
-
+# Taking input from the terminal 
 argumentList = sys.argv
 string1 = sys.argv[1]
 string2 = sys.argv[2]
-print(string1)
-print(string2)
+# Giving the final output based on the flag received from the checkString() method
 flag = checkstring(string1, string2)
 if flag:
     print("True")
