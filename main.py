20f822c0a71c34081770083ba82656100e856dd3
Saurabh Tyagi <60972318+saurtyag@users.noreply.github.com>
2020-03-29 22:39:18 -0500

    Create main.py
    First commit to branch 1

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def checkstring(a: str, b: str) -> bool:
+    len1 = len(set(b))
+    len2 = len(set(a))
+    len3 = len(set(zip(a, b)))
+    boolval = (len1 == len2 == len3)
+    return boolval
+
+
+argumentList = sys.argv
+string1 = sys.argv[1]
+string2 = sys.argv[2]
+print(string1)
+print(string2)
+flag = checkstring(string1, string2)
+if flag:
+    print("True")
+else:
+    print("False")
